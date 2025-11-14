625d38eb0a1ce1be554b5719c539ae926f60bee5
mcastillo <mcastill1@stevens.edu>
2025-11-14 13:24:29 -0500

    Upd 2

--- game.py
+++ game.py
@@ -12,21 +12,23 @@ class Game:
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
         self.img.set_colorkey((0, 0, 0))
 
-        self.img_pos = [160, 260]
+        self.img_pos = [160, 260]           # [x-coord, y-coord(neg)] Origin (0, 0) is top-left corner of screen
         self.movement = [False, False]
+
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))            # Rerenders the screen with a blue/green hue
 
-            # Lemons
-            #Lemon
 
             # Movement logic: img_pos[1] is the y-coord
             # 
             self.img_pos[1] += (-1*self.movement[0] + self.movement[1]) * 5
 
             self.screen.blit(self.img, self.img_pos)
+
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
             
             for event in pygame.event.get():            # Gets user input
                 if event.type == pygame.QUIT:               # User clicks the x on the window
@@ -43,6 +45,7 @@ class Game:
                         self.movement[0] = False
                     if event.key == pygame.K_s:
                         self.movement[1] = False
+
             pygame.display.update()                     
             self.clock.tick(60)                              # Forces loop to run at 60 FPS
 
