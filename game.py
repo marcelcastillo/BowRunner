cf1e34bb1ea975f2ce8f62eb2a60b7e46d71a296
mcastillo <mcastill1@stevens.edu>
2025-11-14 12:43:52 -0500

    Initial commit

---
+++ game.py
@@ -0,0 +1,48 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Bowrunner')        
+        self.screen = pygame.display.set_mode((640, 480))    # Creates the window (resolution in pixels)
+
+        self.clock = pygame.time.Clock()                     # Initialize clock
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0))
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))            # Rerenders the screen with a blue/green hue
+
+
+
+            # Movement logic: img_pos[1] is the y-coord
+            # 
+            self.img_pos[1] += (-1*self.movement[0] + self.movement[1]) * 5    
+
+            self.screen.blit(self.img, self.img_pos)
+            
+            for event in pygame.event.get():            # Gets user input
+                if event.type == pygame.QUIT:               # User clicks the x on the window
+                    pygame.quit()
+                    sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+            pygame.display.update()                     
+            self.clock.tick(60)                              # Forces loop to run at 60 FPS
+
+Game().run()
